978d61aeb32e4d8d3d79f53e16c0573a90aae491
пеп <@email.com>
2026-06-18 07:21:25 +0300

    второе

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,43 @@
-#include <Arduino.h>#include <Arduino.h>
+#include <Arduino.h>
 
-// LED_BUILTIN - константа встроенного светодиода
-#define LED_PIN LED_BUILTIN
+// Условная компиляция для разных плат
+#if defined(BOARD_UNO_R4)
+  #define BUTTON_PIN 2
+  #define LED_PIN LED_BUILTIN
+#elif defined(BOARD_ESP32)
+  #define BUTTON_PIN 0
+  #define LED_PIN 2
+#else
+  #define BUTTON_PIN 2
+  #define LED_PIN LED_BUILTIN
+#endif
+
+bool ledState = false;
+bool lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
+    Serial.begin(115200);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
     pinMode(LED_PIN, OUTPUT);
-    Serial.begin(115200); // Используем высокую скорость передачи
+    Serial.println("System started. Press button to toggle LED.");
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("LED ON");
-    delay(1000);
+    bool currentButtonState = digitalRead(BUTTON_PIN);
 
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("LED OFF");
-    delay(1000);
+    if (currentButtonState != lastButtonState) {
+        lastDebounceTime = millis();
+    }
+
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+        if (currentButtonState == LOW && lastButtonState == HIGH) {
+            ledState = !ledState;
+            digitalWrite(LED_PIN, ledState);
+            Serial.println(ledState ? "LED ON" : "LED OFF");
+        }
+    }
+
+    lastButtonState = currentButtonState;
 }
